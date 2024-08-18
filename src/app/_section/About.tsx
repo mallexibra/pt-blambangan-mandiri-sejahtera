4f237da9e60a972a8552c6c3b73dad15f152034e
mallexibra <mallexibra@gmail.com>
2024-08-18 20:06:39 +0700

    feat: about section

--- src/app/_section/About.tsx
+++ src/app/_section/About.tsx
@@ -1,7 +1,35 @@
+import Container from "@/components/Container";
+import Image from "next/image";
+import LawyerImage from "../../assets/img/lawyers.jpg";
+
 export default function About() {
   return (
-    <div>
-      <p>Ini about section page website</p>
+    <div className="min-h-screen flex items-center">
+      <Container>
+        <h3 className="text-3xl font-bold text-primary mb-7">Tentang Kami</h3>
+        <div className="flex gap-5 justify-between items-center">
+          <section className="w-1/3">
+            <Image
+              src={LawyerImage}
+              className="rounded-md"
+              alt="PT Blambangan Mandiri Sejahtera"
+            />
+          </section>
+          <section className="w-2/3">
+            <p className="font-bold text-primary text-2xl mb-3">
+              PT BLAMBANGAN MANDIRI SEJAHTERA
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero,
+              velit deleniti odit soluta recusandae perferendis? Ipsum aliquam
+              ipsa recusandae nesciunt omnis saepe aperiam dignissimos quas
+              labore adipisci ducimus sequi, quidem reiciendis eligendi delectus
+              rem iure est temporibus voluptatem suscipit quae! Nam ut veniam,
+              tenetur soluta voluptatum corporis necessitatibus laborum labore!
+            </p>
+          </section>
+        </div>
+      </Container>
     </div>
   );
 }
