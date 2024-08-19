8f7357467a5c3e330d0cb090b065d625be82a90b
mallexibra <mallexibra@gmail.com>
2024-08-19 18:20:24 +0700

    chore: set SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "PT Blambangan Mandiri Sejahtera",
-  description: "PT Blambangan Mandiri Sejahtera",
-};
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -12,7 +8,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="id">
+      <Head>
+        <meta name="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="keywords"
+          content="PT Blambangan Mandiri Sejahtera, Advokat, Pengacara, Layanan Hukum, Konsultasi Hukum"
+        />
+        <meta
+          name="description"
+          content="PT Blambangan Mandiri Sejahtera adalah perusahaan yang bergerak di bidang advokat dan layanan hukum, berkomitmen untuk memberikan konsultasi dan layanan hukum terbaik."
+        />
+        <meta
+          property="og:site_name"
+          content="PT Blambangan Mandiri Sejahtera"
+        />
+        <meta property="og:url" content="https://blambanganmandiri.com/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://blambanganmandiri.com/logo.png"
+        />
+        <title>PT Blambangan Mandiri Sejahtera</title>
+      </Head>
       <body>{children}</body>
     </html>
   );
